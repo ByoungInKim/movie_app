8f595126ba7b2b8bb8bfa338b677c654473baea9
ByoungInKim <jojoman200@gmail.com>
2019-05-29 00:37:42 +0900

    Updates

---
+++ precache-manifest.b37d86c8889215610dbf34b8b0cafea2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbec332ae2a662827b40c252a523635d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4a65eebf123b2a1b41d3",
+    "url": "/movie_app/static/css/main.ffc5c85a.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movie_app/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "4a65eebf123b2a1b41d3",
+    "url": "/movie_app/static/js/main.b0c19ad6.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
